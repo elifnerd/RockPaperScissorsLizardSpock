32ca4c9db37f713f268aaf1e9916f2ea00706119
elifnerd <elisabethanne81@gmail.com>
2023-06-02 16:27:00 -0400

    editing and trying to make things more uniform and concise

--- game.py
+++ game.py
@@ -39,6 +39,8 @@ def prompt_player():
     time.sleep(1)
     if user_input == 'Y':
         print('Great. Welcome back! Lets get started.')
+        time.sleep(1)
+        print('Your selections are as follows:')
     elif user_input == 'N':
         print('Great. This is much like your classic game of Rock, Paper, Scissors - with a little twist.')
         time.sleep(1)
@@ -60,11 +62,11 @@ def prompt_player():
         time.sleep(1)
         print('and Spock vaporizes Rock.')
         time.sleep(1)
-        print('Lets get started.')
+        print('Your selections are as follows:')
     else:
-        print('Im sorry. I didnt understand.')
+        return 'Im sorry. I didnt understand.'
     while user_input != 'Y' or 'N':
-        print(prompt_player())
+        return prompt_player()
         
 def single_player():
     player_score = 0
@@ -75,33 +77,73 @@ def single_player():
         round_count += 1
         print('Round', round_count, 'Lets go again.')
         
+        
     player_choice = input('Please make your selection...')
     if player_choice == '1':
-        print('You have chosen Rock.')
+        return 'You have chosen Rock.'
     elif player_choice == '2':
-        print('You have chosen Paper.')
+        return 'You have chosen Paper.'
     elif player_choice == '3':
-        print('You have chosen Scissors.')
+        return 'You have chosen Scissors.'
     elif player_choice == '4':
-        print('You have chosen Lizard.')
+        return 'You have chosen Lizard.'
     elif player_choice == '5':
-        print('You have chosen Spock.')
+        return 'You have chosen Spock.'
     else:
-        print('I am sorry. I did not understand. Lets try again.')
+        return 'I am sorry. I did not understand. Lets try again.'
         time.sleep(1)
-        player_choice
+        player_choice()
     ai_choice = AiPlayer().gesture_choices
     if player_choice == ai_choice:
-        print('Its a tie! Lets go again.')
-        single_player()
+        return 'Its a tie! Lets go again.'
+    else:
+        return player_choice()
         
     
 def multiplayer():
+    player_one_score = 0
+    player_two_score = 0
+    round_count = 0
+    
+    while player_one_score < 2 and player_two_score < 2:
+        round_count += 1
+        print('Round', round_count, 'Lets go again.')
+    
     player_one_choice = input('Player One, please make your selection...')
-    player_two_choice = input('Player Two, please make your selection.')
+    if player_one_choice == '1':
+        return 'You have chosen Rock.'
+    elif player_one_choice == '2':
+        return 'You have chosen Paper.'
+    elif player_one_choice == '3':
+        return 'You have chosen Scissors.'
+    elif player_one_choice == '4':
+        return 'You have chosen Lizard.'
+    elif player_one_choice == '5':
+        return 'You have chosen Spock.'
+    else:
+        return 'I am sorry. I did not understand. Lets try again.'
+        time.sleep(1)
+        return player_one_choice()
+        
+    player_two_choice = input('Player Two, please make your selection...')
+    if player_two_choice == '1':
+        return 'You have chosen Rock.'
+    elif player_two_choice == '2':
+        return 'You have chosen Paper.'
+    elif player_two_choice == '3':
+        return 'You have chosen Scissors.'
+    elif player_two_choice == '4':
+        return 'You have chosen Lizard.'
+    elif player_two_choice == '5':
+        return 'You have chosen Spock.'
+    else:
+        return 'I am sorry. I did not understand. Lets try again.'
+        time.sleep(1)
     if player_one_choice == player_two_choice:
         print('Its a tie! Lets go again.')
-        multiplayer()
+    else:
+        return player_two_choice()
+
             
 def run_game():
      players = input('How many players? 1 or 2?')
