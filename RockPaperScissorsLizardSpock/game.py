1fa96b7eaf4d7e7b7acb70f2d50060ba5902f969
elifnerd <122414235+elifnerd@users.noreply.github.com>
2023-06-02 12:35:08 -0400

    Add files via upload
    attempting welcome message and explaining gameplay

---
+++ RockPaperScissorsLizardSpock/game.py
@@ -0,0 +1,13 @@
+#Display welcome -- Write a function that welcomes the player
+#Explain rules of the game -- Write a funct that explains
+#Create a user input funct for game players(will have option for unsatisfactory user input)
+
+class Game:
+    def __init__(self):
+        user_input = input('Welcome, Player. Have you played this game before? Y/N')
+        if user_input == 'Y':
+            print('Great. Welcome back! Lets get started.')
+        elif user_input == 'N':
+            print('Great. This is much like your classic game of Rock, Paper, Scissors - with a little twist. In the classic game, Rock crushes Scissors, Scissors cuts Paper, and Paper covers Rock. Those rules still apply with some additional options. Rock also crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, and Spock vaporizes Rock. Lets get started.')
+        else:
+            print('I am sorry. I didnt understand. Have you played this game before? Please choose Y or N.')  
---
+++ RockPaperScissorsLizardSpock/player.py
@@ -0,0 +1,3 @@
+class Player:
+    def __init__(self):
+        self.choices = ['Rock', 'Paper', 'Scissor', 'Lizard', 'Spock']
