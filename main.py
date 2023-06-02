5298ec6c599100aa375ab48074f264c67fb11245
elifnerd <elisabethanne81@gmail.com>
2023-06-02 12:32:37 -0400

    attempting welcome and explanation of game rules

---
+++ main.py
@@ -0,0 +1,5 @@
+#1. Store all gesture options/choices in a List (as strings). Find a way to use
+#this list in the code (display options, assign ai player a gesture, etc)
+#2. Ensure correct player wins given round based on players choices
+#3. Best of three to determine gamee winner
+#4. Option of single player(human vs AI) or multiplayer(human vs human)
