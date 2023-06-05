7a6ab3bf7bbe2fd9f408cd85f4cb975ed4daff58
elifnerd <elisabethanne81@gmail.com>
2023-06-05 13:30:27 -0400

    messing around with game play, trying to find the simplest and shortest way to write this

--- game.py
+++ game.py
@@ -3,70 +3,75 @@
 #Create a user input funct for game players(will have option for unsatisfactory user input)
 import time
 
+import random
+
 from ai import AiPlayer
 
-def winner_winner(player_choice, ai_choice):
-    if player_choice == ai_choice:
+def winner_winner(player_one_choice, player_two_choice):
+    if player_one_choice == player_two_choice:
         return 'Its a tie! Go again.'
-    elif player_choice == '1':
-        if ai_choice == 'Scissors' or 'Lizard':
-            return 'Player One wins the round.'
+    elif player_one_choice == '1':
+        if player_two_choice == '3' or player_two_choice == '4':
+            return 'Player One wins!'
         else:
-            return 'AI wins the round.'
-    elif player_choice == '2':
-        if ai_choice == 'Rock' or 'Spock':
-            return 'Player One wins the round.'
+            return 'Player Two wins'
+    elif player_one_choice == '2':
+        if player_two_choice == '1' or player_two_choice == '5':
+            return 'Player One wins!'
         else:
-            return 'AI wins the round.'
-    elif player_choice == '3':
-        if ai_choice == 'Paper' or 'Lizard':
-            return 'Player One wins the round.'
+            return 'Player Two wins!'
+    elif player_one_choice == '3':
+        if player_two_choice == '2' or player_two_choice == '4':
+            return 'Player One wins!'
         else:
-            return 'AI wins the round.'
-    elif player_choice == '4':
-        if ai_choice == 'Spock' or 'Paper':
-            return 'Player One wins the round.'
+            return 'Player Two wins!'
+    elif player_one_choice == '4':
+        if player_two_choice == '2' or player_two_choice == '5':
+            return 'Player One wins!'
         else:
-            return 'AI wins the round.'
-    elif player_choice == '5':
-        if ai_choice == 'Scissors' or 'Rock':
-            return 'Player One wins the round'
+            return 'Player Two wins!'
+    elif player_one_choice == '5':
+        if ai_choice == 'Scissors' or ai_choice == 'Rock':
+            return 'Player One wins!'
         else:
-            return 'AI wins the round.'
+            return 'Player Two wins!'
+        
+def play_game():
+    player_one_score = 0
+    player_two_score = 0
+    
+        
+def display_gameplay_rules():
+    print('Great. This is much like your classic game of Rock, Paper, Scissors - with a little twist.')
+    time.sleep(1)
+    print('In the classic game, Rock crushes Scissors, Scissors cuts Paper, and Paper covers Rock.')
+    time.sleep(1)
+    print('Those rules still apply with some additional options.')
+    time.sleep(1)
+    print('Rock also crushes Lizard,')
+    time.sleep(1)
+    print('Lizard poisons Spock,')
+    time.sleep(1)
+    print('Spock smashes Scissors,')
+    time.sleep(1)
+    print('Scissors decapitates Lizard,')
+    time.sleep(1)
+    print('Lizard eats Paper,')
+    time.sleep(1)
+    print('Paper disproves Spock,')
+    time.sleep(1)
+    print('and Spock vaporizes Rock.')
+    time.sleep(1)
 
 def prompt_player():
     user_input = input('Welcome, Player. Have you played this game before? Y/N')
-    time.sleep(1)
     if user_input == 'Y':
         print('Great. Welcome back! Lets get started.')
-        time.sleep(1)
-        print('Your selections are as follows:')
     elif user_input == 'N':
-        print('Great. This is much like your classic game of Rock, Paper, Scissors - with a little twist.')
-        time.sleep(1)
-        print('In the classic game, Rock crushes Scissors, Scissors cuts Paper, and Paper covers Rock.')
-        time.sleep(1)
-        print('Those rules still apply with some additional options.')
-        time.sleep(1)
-        print('Rock also crushes Lizard,')
-        time.sleep(1)
-        print('Lizard poisons Spock,')
-        time.sleep(1)
-        print('Spock smashes Scissors,')
-        time.sleep(1)
-        print('Scissors decapitates Lizard,')
-        time.sleep(1)
-        print('Lizard eats Paper,')
-        time.sleep(1)
-        print('Paper disproves Spock,')
-        time.sleep(1)
-        print('and Spock vaporizes Rock.')
-        time.sleep(1)
-        print('Your selections are as follows:')
+        print(display_gameplay_rules())
     else:
         return 'Im sorry. I didnt understand.'
-    while user_input != 'Y' or 'N':
-        return prompt_player()
+        user_input()
         
 def single_player():
     player_score = 0
@@ -77,27 +82,33 @@ def single_player():
         round_count += 1
         print('Round', round_count, 'Lets go again.')
         
-        
+    gesture_choices = ['Rock', 'Paper', 'Scissors', 'Lizard', 'Spock']    
     player_choice = input('Please make your selection...')
-    if player_choice == '1':
-        return 'You have chosen Rock.'
-    elif player_choice == '2':
-        return 'You have chosen Paper.'
-    elif player_choice == '3':
-        return 'You have chosen Scissors.'
-    elif player_choice == '4':
-        return 'You have chosen Lizard.'
-    elif player_choice == '5':
-        return 'You have chosen Spock.'
+    ai_choice = random.choice(gesture_choices)
+
+    print('You have chosen', player_choice)
+    print('AI has chosen', ai_choice)
+
+    winner = winner_winner(player_choice, ai_choice)
+    
+    if winner == 'tie':
+        print('Its a tie!')
+    elif winner == 'Player One':
+        print('Player One wins this round.')
+        player_score += 1
     else:
-        return 'I am sorry. I did not understand. Lets try again.'
+        print('AI wins this round.')
+        ai_score += 1
+        print('Player Ones score is', player_score)
         time.sleep(1)
-        player_choice()
-    ai_choice = AiPlayer().gesture_choices
-    if player_choice == ai_choice:
-        return 'Its a tie! Lets go again.'
-    else:
-        return player_choice()
+        print('AIs score is', ai_score)
+        print()
+        
+        if player_score > ai_score:
+            print('Congratulations, Player One! You have won the game.')
+        else:
+            print('Boo, AI has won the game. Better luck next time.')
+        
         
     
 def multiplayer():
@@ -110,67 +121,29 @@ def multiplayer():
         print('Round', round_count, 'Lets go again.')
     
     player_one_choice = input('Player One, please make your selection...')
-    if player_one_choice == '1':
-        return 'You have chosen Rock.'
-    elif player_one_choice == '2':
-        return 'You have chosen Paper.'
-    elif player_one_choice == '3':
-        return 'You have chosen Scissors.'
-    elif player_one_choice == '4':
-        return 'You have chosen Lizard.'
-    elif player_one_choice == '5':
-        return 'You have chosen Spock.'
-    else:
-        return 'I am sorry. I did not understand. Lets try again.'
-        time.sleep(1)
-        return player_one_choice()
+
+    print('Player One has chosen', player_one_choice)
+    time.sleep(1)
         
     player_two_choice = input('Player Two, please make your selection...')
-    if player_two_choice == '1':
-        return 'You have chosen Rock.'
-    elif player_two_choice == '2':
-        return 'You have chosen Paper.'
-    elif player_two_choice == '3':
-        return 'You have chosen Scissors.'
-    elif player_two_choice == '4':
-        return 'You have chosen Lizard.'
-    elif player_two_choice == '5':
-        return 'You have chosen Spock.'
+    
+    print('Player Two has chosen', player_two_choice)
+    time.sleep(1)
+    
+    winner = winner_winner(player_one_choice, player_two_choice)
+    if winner == 'tie':
+        print('Its a tie!')
+    elif winner == 'Player One':
+        print('Player One wins this round.')
+        player_one_score += 1
     else:
-        return 'I am sorry. I did not understand. Lets try again.'
-        time.sleep(1)
-    if player_one_choice == player_two_choice:
-        print('Its a tie! Lets go again.')
+        print('Player Two wins this round.')
+        player_two_score += 1
+        
+    print ('Player Ones score is', player_one_score)
+    print('Player Twos score is', player_two_score)
+    
+    if player_one_score > player_two_score:
+        print('Congratulations, Player One! You have won the game.')
     else:
-        return player_two_choice()
-
-            
-def run_game():
-     players = input('How many players? 1 or 2?')
-     time.sleep(1)
-     if players == '1':
-         print('Wonderful. Your options are as follows.')
-         time.sleep(1)
-         print('Choose 1 for Rock')
-         time.sleep(1)
-         print('Choose 2 for Paper')
-         time.sleep(1)
-         print('Choose 3 for Scissors')
-         time.sleep(1)
-         print('Choose 4 for Lizard')
-         time.sleep(1)
-         print('Choose 5 for Spock')
-         single_player()
-     elif players == '2':
-         print('Wonderful. Your options are as follows.')
-         time.sleep(1)
-         print('Choose 1 for Rock')
-         time.sleep(1)
-         print('Choose 2 for Paper')
-         time.sleep(1)
-         print('Choose 3 for Scissors')
-         time.sleep(1)
-         print('Choose 4 for Lizard')
-         time.sleep(1)
-         print('Choose 5 for Spock')
-         multiplayer()
+        print('Congratulations, Player Two! You have won the game.')
